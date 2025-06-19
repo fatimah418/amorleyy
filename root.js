2d05b478466a3e0adf5fe3eb4bc535538e4651f0
fatimah418 <timahamorley@gmail.com>
2025-06-19 14:03:48 +0100

    Update root.js

--- root.js
+++ root.js
@@ -1,4 +1,4 @@
-function greetings (x, y, value){
+function greetings(x, y, value){
     let x = Number(prompt("What is your first nmber"))
     let y = prompt("is frankin a goat")
     let value = prompt("am i gay")
