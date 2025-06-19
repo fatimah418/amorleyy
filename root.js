768b2989509db1325407c2c0bcef544be5500800
fatimah418 <timahamorley@gmail.com>
2025-06-19 14:03:03 +0100

    Add files via upload

---
+++ root.js
@@ -0,0 +1,9 @@
+function greetings (x, y, value){
+    let x = Number(prompt("What is your first nmber"))
+    let y = prompt("is frankin a goat")
+    let value = prompt("am i gay")
+    alert(`Your first mumber is ${x}`)
+    alert(`${y}, he is(n't)`)
+    alert(`${value}`)
+}
+
