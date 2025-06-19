06ba571d6274774f34ae702809f9be981936a0a1
fatimah418 <timahamorley@gmail.com>
2025-06-19 14:05:34 +0100

    Update root.js

--- root.js
+++ root.js
@@ -1,7 +1,7 @@
 function greetings(x, y, value){
-    let x = Number(prompt("What is your first nmber"))
-    let y = prompt("is frankin a goat")
-    let value = prompt("am i gay")
+    x = Number(prompt("What is your first nmber"))
+    y = prompt("is frankin a goat")
+    value = prompt("am i gay")
     alert(`Your first mumber is ${x}`)
     alert(`${y}, he is(n't)`)
     alert(`${value}`)
